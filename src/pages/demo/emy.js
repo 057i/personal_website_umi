45e204df2fb7e962725d59e2e9ffc909e700816a
057i <fz12580@hotmail.com>
2019-12-15 20:47:40 +0800

    modify_2019-12-15

--- src/pages/demo/emy.js
+++
@@ -1,9 +0,0 @@
-export default function emy() {
-  console.log(111);
-  return (<>
-    <div>
-      <h1>fdsajsajdl</h1>
-    </div>
-  </>);
-
-}
--- src/pages/future.js
+++ src/pages/future.js
@@ -1,4 +1,15 @@
-// export default function Future() {
-//   return (<>dasds</>);
-//
-// }
+import styles from '@/pages/contact.css';
+
+export default function Future(props) {
+  return (<>
+    <header className={styles.contactHeader}>
+      <span onClick={() => jumpToIndex(props, '/')} className={styles.navigation}>>>57的个人站点</span>
+    </header>
+    <h1 style={{ textAlign: 'center' }}>未来拥有无限的可能</h1>
+  </>);
+
+}
+
+function jumpToIndex(props, add) {
+  props.history.push(add);
+}
